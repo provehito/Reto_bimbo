5833e685b1a74f6480add674af64c102d6ee05f7
provehito <h.rueda86@gmail.com>
2016-07-30 12:31:32 -0500

    second commit with sql file

---
+++ mysql.sql
@@ -0,0 +1,8 @@
+﻿
+
+select * from bimbo_dataset.producto a, bimbo_dataset.train b, bimbo_dataset.cliente c, bimbo_dataset.town_state d 
+where a.producto_id = b.producto_id 
+and b.cliente_id = c.cliente_id 
+and b.agencia_id = d.agencia_id 
+and (c.nombre_cliente like '%XX%' or c.nombre_cliente like '%xx%')
+
